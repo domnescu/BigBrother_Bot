411f5c3151467896b926e590fc2b6a0f4807aee5
Maxim Domnescu <maxim.domnescu@yandex.ru>
2020-09-14 17:24:02 +0300

    Добавление ссылки на Исходный Код бота

---
+++ BigBrother_V2/Vkontakte/Commands/ReferencesToBigBrother/Source.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace BigBrother_V2.Vkontakte.Commands.ReferencesToBigBrother
+{
+    class Source:Command
+    {
+        public override string Name => "Отправка ссылки на исходники бота";
+
+        MessagesSendParams @params = new MessagesSendParams();
+
+        public override void Execute(Message message, VkApi client)
+        {
+            @params.PeerId = message.PeerId.Value;
+            @params.RandomId = new Random().Next();
+            @params.Message = "Мой исходный код доступен по ссылке: https://github.com/domnescu/BigBrother_Bot";
+            Send(@params, client);
+        }
+
+        public override bool Contatins(Message message)
+        {
+            string text = message.Text.ToLower();
+            Database db = new Database();
+            if ((text.Contains("исходник") || (text.Contains("код") && text.Contains("исходный"))) && db.CheckText(text,"BotNames"))
+                return true;
+            return false;
+        }
+    }
+}

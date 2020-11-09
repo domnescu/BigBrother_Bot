7fc62159b50504e86677094d35e6a3aa0e64453e
Maxim Domnescu <maxim.domnescu@yandex.ru>
2020-11-09 18:15:39 +0300

    Добавление реакции на @all

---
+++ BigBrother_V2/Vkontakte/Commands/Other/PushAll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace BigBrother_V2.Vkontakte.Commands
+{
+    /// <summary>
+    /// Данная функция не входит в релизную версию, она используется исключительно для тестирования функций
+    /// </summary>
+    class PushAll : Command
+    {
+        public override string Name => "Тестовая команда";
+
+        MessagesSendParams @params = new MessagesSendParams();
+
+        public override void Execute(Message message, VkApi client)
+        {
+            @params.PeerId = message.PeerId.Value;
+            @params.RandomId = new Random().Next();
+            User user = new User(message.FromId.Value, client);
+            @params.Message = user.FirstName + ", объясни мне пожалуйста, нахуя ты это сделал?";
+            Send(@params, client);
+        }
+
+        public override bool Contatins(Message message)
+        {
+            string text = message.Text.ToLower();
+            if (text.Contains("@all"))
+                return true;
+            return false;
+        }
+    }
+}

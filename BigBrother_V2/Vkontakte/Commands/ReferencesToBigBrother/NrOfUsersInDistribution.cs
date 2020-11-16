9fb041e0cfe7e2b1f4eff72070e2bb0da705cf74
Maxim Domnescu <maxim.domnescu@yandex.ru>
2020-11-16 12:13:02 +0300

    Обновление Функции подсчёта людей которые потенциально подписаны на рассылку

--- BigBrother_V2/Vkontakte/Commands/ReferencesToBigBrother/NrOfUsersInDistribution.cs
+++ BigBrother_V2/Vkontakte/Commands/ReferencesToBigBrother/NrOfUsersInDistribution.cs
@@ -21,14 +21,27 @@ namespace BigBrother_V2.Vkontakte.Commands.ReferencesToBigBrother
             @params.RandomId = new Random().Next();
             int users = 0;
             int Chats =0;
+            List<long> ChatsForNrOfUsers = new List<long>();
             foreach(var PeerID in conversations)
             {
                 if (PeerID < 2000000000)
                     users++;
                 else
+                {
                     Chats++;
+                    ChatsForNrOfUsers.Add(PeerID);
+                }
             }
-            @params.Message = "Сейчас на мою рассылку подписаны " + users + " людей и "+ Chats + " бесед";
+            @params.Message = "Сейчас на мою рассылку подписаны " + users + " людей и "+ Chats + " бесед.";
+            var chats = client.Messages.GetConversationsById(ChatsForNrOfUsers);
+
+            foreach (var chat in chats.Items)
+            {
+
+                var UsersInChat = client.Messages.GetConversationMembers(chat.Peer.Id);
+                long NrOFUsersInChat = UsersInChat.Count;
+                @params.Message += "\n" + chat.ChatSettings.Title + ":" + NrOFUsersInChat +" участников";
+            }
             Send(@params, client);
         }
 

c1abd9a4c8e6727b3101d7e58b1df3b15d0cfde1
Maxim Domnescu <maxim.domnescu@yandex.ru>
2021-01-11 14:42:37 +0300

    Исправление грамматической ошибки

--- BigBrother_V2/Vkontakte/Commands/Other/Nuran.cs
+++ BigBrother_V2/Vkontakte/Commands/Other/Nuran.cs
@@ -14,7 +14,7 @@ namespace BigBrother_V2.Vkontakte.Commands.Other
 
         public override void Execute(Message message, VkApi client)
         {
-            @params.Message = "[id475894534|Нуран] тут главный по снюсу, всё что связанно со снюсом, это к нему";
+            @params.Message = "[id475894534|Нуран] тут главный по снюсу, всё что связано со снюсом, это к нему";
             @params.DisableMentions = true;
             @params.PeerId = message.PeerId.Value;
             @params.RandomId = new Random().Next();
